d5506bf938e0884da110de91d807b25fe2fc24eb
Alex Puzyk <APuzyk@gmail.com>
2020-09-08 15:34:33 -0700

    Documentation updates

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use std::env;
 
+/// Runs [deepwalk](https://arxiv.org/abs/1403.6652) on a whitespace separated edge list.
+/// 
+/// Paramters, including the input file are provided in
+/// a json file the location of which is passed as an 
+/// argument to the CLI.  
+/// 
+/// # Examples
+/// 
+/// ```
+/// deepwalk config.json
+/// ```
 fn main() {
     deepwalk::deepwalk(env::args().collect())
 }
