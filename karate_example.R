c59859a8a28a5e4610d9e2c631802ec0db82aff6
Alex Puzyk <APuzyk@gmail.com>
2020-09-19 10:00:45 -0700

    Fixes for R script and updating gitignore to ignore history

--- karate_example.R
+++ karate_example.R
@@ -1,9 +1,11 @@
+library(ggplot2)
+library(data.table)
 weights <- data.table::fread("karate_weights.txt", sep = " ", header=FALSE)
 names(weights) <- c("node_id", paste0("v", c(1:(ncol(weights)-1))))
 karate_edges <- data.table::fread("karate_network.txt")
 communities <- data.table::fread("karate_communities.txt")
 weights <- merge(weights, communities)
-segments <- data.table::as.data.table(t(apply(karate, 1, function(x) unlist(lapply(x, function(y) weights[node_id==y, c(2, 3)])))))
+segments <- data.table::as.data.table(t(apply(karate_edges, 1, function(x) unlist(lapply(x, function(y) weights[node_id==y, c(2, 3)])))))
 names(segments) <- paste0("V", c(1:ncol(segments)))
 weights[, community:=as.factor(community)]
 p <- ggplot() + 
